4c53a389b3eba8fe272bc863a44810b267a43d39
elisammg <elisamonzon@outlook.com>
2020-03-31 15:50:03 -0600

    almost success

--- src/main/java/com/proyecto/fabrica/FabricaApplication.java
+++ src/main/java/com/proyecto/fabrica/FabricaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class FabricaApplication {
 
-	@RequestMapping("/listar")
+	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
