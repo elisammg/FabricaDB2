ea02701e34acb3783428b2b81479f216326afd0c
elisammg <elisamonzon@outlook.com>
2020-04-05 17:01:46 -0600

    Sin Login

--- src/main/java/com/proyecto/fabrica/interfaces/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.fabrica.interfaces;
-
-import com.proyecto.fabrica.modelo.Users;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UsersRepository extends MongoRepository<Users, String> {
-    Users findByUsername(String username);
-}
